6aa40e063a7bd4e53f8b023d8ef597b73bb9e749
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-06-25 14:02:05 +0100

    select reward button linked

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const bookMarked = document.getElementById("bookmarked");
 const bookText = document.getElementById("bookmark-text");
+const selectThis = document.getElementById("select-this");
+const success = document.getElementById("success");
+const htmlDoc = document.documentElement;
+const gotIt = document.getElementById("got-it");
 
 bookMarked.onclick = () => {
     bookMarked.classList.toggle("active-bookmark");
 }
 
+selectThis.onclick = () => {
+    success.style.display = "flex";
+    htmlDoc.style.overflow = "hidden";
+    
+    window.onclick = function(event) {
+        if (event.target === success) {
+            htmlDoc.style.overflow = "visible";
+            success.style.display = "none";
+        }
+    }
+
+    gotIt.addEventListener("click", () => {
+        htmlDoc.style.overflow = "visible";
+        success.style.display = "none";
+    })
+
+}
 
 
 // const visibleClass = document.getElementById("visible");
