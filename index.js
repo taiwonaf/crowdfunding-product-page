f949d4198a65a42240230a8f5e53bedafdd4dabb
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-06-25 20:50:27 +0100

    Interaction completed

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const selectThis = document.getElementById("select-this");
 const success = document.getElementById("success");
 const htmlDoc = document.documentElement;
 const gotIt = document.getElementById("got-it");
+const backThis = document.getElementById("back-this");
+const selectionModal = document.getElementById("selection-modal");
+const docBody = document.body;
+const visibleClasses = document.querySelectorAll(".visible");
+const toggleBtn = document.getElementById("toggle");
+const cancelSelect = document.getElementById("cancel");
+
+cancelSelect.onclick = () => {
+    selectionModal.style.display = "none";
+    window.onclick = function(event) {
+        if (event.target === selectionModal) {
+            htmlDoc.style.overflow = "visible";
+            selectionModal.style.display = "none";
+        }
+    }
+}
+
+visibleClasses.forEach((visibleClass) => {
+    visibleClass.addEventListener("click", (e) => {
+        visibleClass.classList.toggle("activate-hidden");
+        console.log(e.target);
+        // if (e.target == toggleBtn) {
+        //     visibleClass.classList.toggle("activate-hidden");
+        // }
+    })
+})
 
 bookMarked.onclick = () => {
     bookMarked.classList.toggle("active-bookmark");
@@ -24,8 +50,15 @@ selectThis.onclick = () => {
         htmlDoc.style.overflow = "visible";
         success.style.display = "none";
     })
-
 }
+
+backThis.addEventListener("click", () => {
+    selectionModal.style.display = "flex";
+    htmlDoc.style.overflow = "scroll";
+    htmlDoc.style.height = "1400px";
+    docBody.style.height = "1400px"
+    docBody.style.overflow = "hidden"
+})
 
 
 // const visibleClass = document.getElementById("visible");
